5e3109fbe4a01713ff609b2fe59f3464df4984ce
liujunwei <liujunwei0422@icloud.com>
2020-02-13 11:50:50 +0800

    NSMutableArray debug

--- JWBasicFoundation.podspec
+++ JWBasicFoundation.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'JWBasicFoundation'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3'
   s.summary          = 'A short description of JWBasicFoundation.'
 
 # This description is used to generate tags and improve search results.
--- JWBasicFoundation/Classes/NSArray/NSMutableArray/NSMutableArray+JW.m
+++ JWBasicFoundation/Classes/NSArray/NSMutableArray/NSMutableArray+JW.m
@@ -12,19 +12,19 @@
 
 #pragma mark - safe
 #pragma mark Add
-- (void)ljw_safeAddObject:(id)object {
+- (void)jw_safeAddObject:(id)object {
     if (object) {
         [self addObject:object];
     }
 }
 
-- (void)ljw_safeAddObjectsFromArray:(NSArray *)array {
+- (void)jw_safeAddObjectsFromArray:(NSArray *)array {
     if (array.count) {
         [self addObjectsFromArray:array];
     }
 }
 
-- (BOOL)ljw_addObject:(id)object {
+- (BOOL)jw_addObject:(id)object {
     if (object) {
         [self addObject:object];
         return YES;
@@ -33,62 +33,62 @@
 }
 
 #pragma mark Insert
-- (void)ljw_safeInsertObject:(id)object atIndex:(NSUInteger)index {
+- (void)jw_safeInsertObject:(id)object atIndex:(NSUInteger)index {
     if (object && index <= self.count) {
         [self insertObject:object atIndex:index];
     }
 }
-- (void)ljw_safeInsertObjects:(NSArray *)objects atIndexes:(NSIndexSet *)indexes {
+- (void)jw_safeInsertObjects:(NSArray *)objects atIndexes:(NSIndexSet *)indexes {
     if (objects.count && indexes) {
         [self insertObjects:objects atIndexes:indexes];
     }
 }
 
 #pragma mark remove
-- (void)ljw_safeRemoveObject:(id)object {
+- (void)jw_safeRemoveObject:(id)object {
     if (object && [self containsObject:object]) {
         [self removeObject:object];
     }
 }
 
-- (void)ljw_safeRemoveLastObject {
+- (void)jw_safeRemoveLastObject {
     if (self.count) {
         [self removeLastObject];
     }
 }
 
-- (void)ljw_safeRemoveObject:(id)object inRange:(NSRange)range {
+- (void)jw_safeRemoveObject:(id)object inRange:(NSRange)range {
     if (object && range.location + range.length < self.count) {
         [self removeObject:object inRange:range];
     }
 }
 
-- (void)ljw_safeRemoveObjectAtIndex:(NSUInteger)index {
+- (void)jw_safeRemoveObjectAtIndex:(NSUInteger)index {
     if (index < self.count) {
         [self removeObjectAtIndex:index];
     }
 }
 
-- (void)ljw_safeRemoveObjectIdenticalTo:(id)object inRange:(NSRange)range {
+- (void)jw_safeRemoveObjectIdenticalTo:(id)object inRange:(NSRange)range {
     if (object && range.location + range.length < self.count) {
-        [self ljw_safeRemoveObjectIdenticalTo:object inRange:range];
+        [self jw_safeRemoveObjectIdenticalTo:object inRange:range];
     }
 }
 
-- (void)ljw_safeRemoveObjectsAtIndexes:(NSIndexSet *)indexes {
+- (void)jw_safeRemoveObjectsAtIndexes:(NSIndexSet *)indexes {
     if (indexes) {
-        [self ljw_safeRemoveObjectsAtIndexes:indexes];
+        [self jw_safeRemoveObjectsAtIndexes:indexes];
     }
 }
 
 #pragma mark Replace
-- (void)ljw_safeReplaceObjectAtIndex:(NSUInteger)index withObject:(id)object {
+- (void)jw_safeReplaceObjectAtIndex:(NSUInteger)index withObject:(id)object {
     if (object && index < self.count) {
         [self replaceObjectAtIndex:index withObject:object];
     }
 }
 
-- (void)ljw_safeReplaceObjectsAtIndexex:(NSIndexSet *)indexes withObjects:(NSArray *)objects {
+- (void)jw_safeReplaceObjectsAtIndexex:(NSIndexSet *)indexes withObjects:(NSArray *)objects {
     if (objects.count && indexes) {
         [self replaceObjectsAtIndexes:indexes withObjects:objects];
     }
